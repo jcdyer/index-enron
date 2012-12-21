0b060ee6d4241371390e84403287d78fc596cef4
J. Cliff Dyer <jcd@mint>
2012-12-21 07:15:25 -0800

    Created a file pooling mechanism to speed up write access.

---
+++ files.py
@@ -0,0 +1,22 @@
+
+class FilePool(object):
+    
+    def __init__(self, limit):
+        self.limit = limit
+        self.pool = {}
+        self.size = 0
+
+    def write(self, filename, content):
+        if filename not in self.pool:
+            if self.size >= self.limit:
+                self.clear_pool()
+            self.pool[filename] = open(filename, 'a')
+        self.pool[filename].write(content)
+
+    def clear_pool(self):
+        for filename in self.pool.keys():
+            self.close(filename)
+
+    def close(self, filename):
+            self.pool[filename].close()
+            del self.pool[filename]
